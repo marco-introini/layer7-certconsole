1c0bba17866ab91c3a33b1bf063dc85c3c9d85ec
Marco "Mint" Introini <mint.dev@pm.me>
2024-11-21 16:35:17 +0100

    Add table to home page

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -2,12 +2,41 @@
 
 namespace App\Livewire;
 
+use App\Models\Certificate;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
 use Livewire\Component;
+use Filament\Tables\Table;
 
-class Home extends Component
+class Home extends Component implements HasForms, HasTable
 {
+    use InteractsWithForms;
+    use InteractsWithTable;
+
+
     public function render()
     {
         return view('livewire.home');
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(Certificate::query())
+            ->columns([
+                TextColumn::make('name'),
+            ])
+            ->filters([
+                // ...
+            ])
+            ->actions([
+                // ...
+            ])
+            ->bulkActions([
+                // ...
+            ]);
     }
 }
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,3 +1,7 @@
-<div>
+<div class="container mx-auto px-4 py-8">
+
+    <div>
+        {{ $this->table }}
+    </div>
 
 </div>

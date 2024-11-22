f314220a3c50bc688d7ce5aaac9c070d26e1be9b
Marco "Mint" Introini <mint.dev@pm.me>
2024-11-22 11:07:51 +0100

    Add label to enum

--- app/Enumerations/CertificateType.php
+++ app/Enumerations/CertificateType.php
@@ -2,10 +2,17 @@
 
 namespace App\Enumerations;
 
-enum CertificateType: string
+use Filament\Support\Contracts\HasLabel;
+
+enum CertificateType: string implements HasLabel
 {
     case TRUSTED_CERT = 'Trusted Certificate';
     case PRIVATE_KEY = 'Private Key';
     case USER_CERTIFICATE = 'User Certificate';
 
+
+    public function getLabel(): ?string
+    {
+        return $this->value;
+    }
 }

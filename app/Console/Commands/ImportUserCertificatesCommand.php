2c964bbb40cdd1a7df0d72b5a4b2a0362912ae0a
Marco "Mint" Introini <mint.dev@pm.me>
2024-11-25 11:06:10 +0100

    Add user certificates retrieval and import functionality
    Implemented logic to fetch user-specific certificates from Layer7 and save them into the application’s database. Introduced a new method in `Layer7Integration` to handle certificate fetching, and updated `ImportUserCertificatesCommand` to utilize this method effectively. Added new entry to `.gitignore` for `run_artisan_with_proxy.sh` script.

--- app/Console/Commands/ImportUserCertificatesCommand.php
+++ app/Console/Commands/ImportUserCertificatesCommand.php
@@ -3,14 +3,10 @@
 namespace App\Console\Commands;
 
 use App\Enumerations\CertificateType;
-use App\Http\Integrations\Layer7\Layer7RestmanConnector;
-use App\Http\Integrations\Layer7\Requests\GatewayUsers;
-use App\Http\Integrations\Layer7\Requests\UserCertificates;
+use App\Http\Integrations\Layer7\Layer7Integration;
 use App\Models\Certificate;
 use App\Models\Gateway;
 use Illuminate\Console\Command;
-use Log;
-use Spatie\SslCertificate\SslCertificate;
 
 class ImportUserCertificatesCommand extends Command
 {
@@ -20,37 +16,13 @@ class ImportUserCertificatesCommand extends Command
 
     public function handle(): void
     {
-        $gateway = Gateway::first();
-        $connector = new Layer7RestmanConnector($gateway);
-        $response = $connector->send(new GatewayUsers($gateway));
+        foreach (Gateway::all() as $gateway) {
+            $layer7 = new Layer7Integration($gateway);
+            $collection = $layer7->getUserCertificates();
 
-        if ($response->status() !== 200) {
-            // error
-            Log::error('Error getting user from Layer7: ' . $response->status(). " - ". $response->body());
-        }
-
-        $reader = $response->xmlReader();
-        //$raw = $response->body();
-
-        $keys = $reader->value('l7:Id')->get();
-        foreach ($keys as $key) {
-            $this->info("Getting certificates for user $key");
-            $certResponse = $connector->send(new UserCertificates($gateway, $key));
-
-            if ($certResponse->status() !== 200) {
-                // error
-                Log::error("Error getting certificate for user $key from Layer7: " . $certResponse->status(). " - ". $certResponse->body());
-            }
-
-            $readerCert = $certResponse->xmlReader();
-
-            $keysCert = $readerCert->value('l7:Encoded')->get();
-
-            foreach ($keysCert as $keyCert) {
-                $pemData = SslCertificate::der2pem(base64_decode($keyCert));
-                Certificate::fromPemCertificate($gateway, CertificateType::USER_CERTIFICATE, $pemData);
+            foreach ($collection as $pemCertificate) {
+                Certificate::fromPemCertificate($gateway, CertificateType::USER_CERTIFICATE, $pemCertificate->pemData);
             }
         }
-
     }
 }
--- app/Http/Integrations/Layer7/Layer7Integration.php
+++ app/Http/Integrations/Layer7/Layer7Integration.php
@@ -5,8 +5,10 @@ namespace App\Http\Integrations\Layer7;
 use App\Collections\PemCertificateCollection;
 use App\DataTransferObjects\PemCertificate;
 use App\Enumerations\CertificateType;
+use App\Http\Integrations\Layer7\Requests\GatewayUsers;
 use App\Http\Integrations\Layer7\Requests\PrivateKeys;
 use App\Http\Integrations\Layer7\Requests\TrustedCertificates;
+use App\Http\Integrations\Layer7\Requests\UserCertificates;
 use App\Models\Certificate;
 use App\Models\Gateway;
 use Illuminate\Support\Facades\Log;
@@ -66,6 +68,43 @@ class Layer7Integration
             $collection->add(new PemCertificate($pemData));
         }
 
+        return $collection;
+    }
+
+    public function getUserCertificates(): PemCertificateCollection
+    {
+        $response = $this->connector->send(new GatewayUsers($this->gateway));
+        $collection = new PemCertificateCollection();
+
+        if ($response->status() !== 200) {
+            // error
+            Log::error('Error getting user from Layer7: '.$response->status()." - ".$response->body());
+            return $collection;
+        }
+
+        $reader = $response->xmlReader();
+        //$raw = $response->body();
+
+        $keys = $reader->value('l7:Id')->get();
+        foreach ($keys as $key) {
+            //$this->info("Getting certificates for user $key");
+            $certResponse = $this->connector->send(new UserCertificates($this->gateway, $key));
+
+            if ($certResponse->status() !== 200) {
+                // error
+                Log::error("Error getting certificate for user $key from Layer7: ".$certResponse->status()." - ".$certResponse->body());
+                return $collection;
+            }
+
+            $readerCert = $certResponse->xmlReader();
+
+            $keysCert = $readerCert->value('l7:Encoded')->get();
+
+            foreach ($keysCert as $keyCert) {
+                $pemData = SslCertificate::der2pem(base64_decode($keyCert));
+                $collection->add(new PemCertificate($pemData));
+            }
+        }
         return $collection;
     }
 

24b2026a8ebb2b397374f946c4ce8ba93f43e851
Marco "Mint" Introini <mint.dev@pm.me>
2025-08-31 08:38:55 +0200

    Upgrade dependencies to Filament v4 and PHP 8.4
    Updated Filament packages to v4 and required PHP version to 8.4 for compatibility with the latest ecosystem upgrades.

--- app/Collections/PemCertificateCollection.php
+++ app/Collections/PemCertificateCollection.php
@@ -2,17 +2,19 @@
 
 namespace App\Collections;
 
+use Override;
+use InvalidArgumentException;
 use App\DataTransferObjects\PemCertificate;
 use Illuminate\Support\Collection;
 
 /** @extends Collection<int,PemCertificate> */
 final class PemCertificateCollection extends Collection
 {
-    #[\Override]
+    #[Override]
     public function add($item): self
     {
         if (!$item instanceof PemCertificate) {
-            throw new \InvalidArgumentException('Item must be an instance of PemCertificate');
+            throw new InvalidArgumentException('Item must be an instance of PemCertificate');
         }
 
         parent::add($item);
